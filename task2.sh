9efffe7410d016b319d55148faa4fe23ac93021a
Your Name <yourname@example.org>
2020-12-30 17:02:13 +0000

    task2

---
+++ task2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+git config --global user.email "{USERNAME_GITHUB}@github.com";
+git config --global user.name "${USERNAME_GITHUB}";
+git config --global pull.rebase false;
+
+checkout_work () {
+  repo_target=$1;
+  folder=$2;
+  git clone "${repo_target}" "${folder}";
+  cd ${folder};
+  cd ..;
+}
+
+
+workrepo=https://$API_TOKEN_GITHUB@github.com/$USERNAME_GITHUB/$REPO_GITHUB.git;
+workdir="work";
+checkout_work ${workrepo} ${workdir};
+cd ${workdir};
+
+echo "Start task1" ;
+chmod +x ./run.sh ;
+./run.sh ;
+
+today=$(date +%FT%T);
+echo "Work done-${today}";
+
