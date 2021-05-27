e62d22be7a0d18ddbf89279f7cb02e4fdadf9b05
Your Name <yourname@example.org>
2021-05-27 13:26:46 +0000

    heartbeat task

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git config --global user.email "{USERNAME1_GITHUB}@github.com";
+git config --global user.name "${USERNAME1_GITHUB}";
+git config --global pull.rebase false;
 
 date=$(date '+%Y-%m-%d');
 echo -en "# actions\n\nunlimited github action minutes in public repository for secret tasks\n\n${date}" > README.md
