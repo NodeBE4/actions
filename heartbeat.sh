276ffed115238b2013b5a0f3015c482c7c0fe6c9
duty machine <your@example.com>
2021-12-06 05:12:26 +0000

    update

--- heartbeat.sh
+++ heartbeat.sh
@@ -7,4 +7,4 @@ date=$(date '+%Y-%m-%d');
 echo -en "# actions\n\nunlimited github action minutes in public repository for secret tasks\n\n${date}" > README.md
 git add README.md
 git commit --allow-empty -m"heartbeat task ${date}";
-git push
+git push origin main
