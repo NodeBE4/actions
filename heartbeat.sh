2a87f96155c316b539db6e086235e333efe11d36
duty machine <your@example.com>
2021-12-07 18:14:26 +0000

    update

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-git config user.name "${USERNAME1_GITHUB}";
-git config user.email "${USERNAME1_GITHUB}@github.com";
-git config pull.rebase false;
+git config --global user.email "{USERNAME1_GITHUB}@github.com";
+git config --global user.name "${USERNAME1_GITHUB}";
+git config --global pull.rebase false;
 
 date=$(date '+%Y-%m-%d');
 echo -en "# actions\n\nunlimited github action minutes in public repository for secret tasks\n\n${date}" > README.md
 git add README.md
 git commit --allow-empty -m"heartbeat task ${date}";
 git push origin main
+
+today=$(date +%FT%T);
+echo "Work done-${today}";
